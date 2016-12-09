dc0fc66bf3fa9eb9b479e22c582185ddf2223a23
ktabriga <jhonatancatabriga@gmail.com>
2016-12-09 10:18:12 -0200

    Ajusta criação de configurações e aplicação da mascara

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
+const specialChars = /[-\.\,\(\)\/]/g;
 
-function mask(x, options) {
-	const chars = String(x).replace(/[-.\,\(\)]/g).split('');	
+function cleanSpecialChars (input) {
+	return String(input).replace(specialChars, '');
+}
+
+function applyMask(input, options) {
+	const chars = cleanSpecialChars(input).split('');
 	return chars.reduce(function (acc, char, i) {
-		console.log(char, i)
 		if (i == 0 && options[0]) return `${options[0]}${char}`;
-		if (options[i]) {
-			console.log(i, acc, char);
-			return `${acc}${options[i]}${char}`;
+		if (options[i + 1]) {
+			return `${acc}${char}${options[i + 1]}`;
 		}
 		return acc + char;
 	}, '')
 }
 
-function createOptions (mask) {
-	const elements = ['-', '.', ',', '(', ')', ' '];
-	return mask.split('').reduce((options, char, i) => {
-		if (elements.includes(char)) {
+
+//Cria configuração com posição dos elementos
+function createMaskOptions (pattern) {
+	return pattern.split('').reduce((options, char, i) => {
+		if (specialChars.test(char)) {
 			const anteriores = Object.keys(options).map(i => options[i]).join('');
 			let position = i === 0 ? 0 : i - anteriores.length;
-			console.log(position, char, anteriores)
-			if (elements.includes(options[position])) {
+			if (specialChars.test(options[position])) {
 				char = options[position] + char;
-			}			
+			}
 			return Object.assign({}, options, {[position]: char});
 		}
 		return options;
 	}, {});
 }
 
+exports.default = {
+	applyMask,
+	cleanSpecialChars,
+	createMaskOptions
+};
